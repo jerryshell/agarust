76c6ea4f3547267076e93d33fdba124d8d74a6f6
jerry <calljerryli@outlook.com>
2025-01-14 01:50:06 +0800

    feat: update server connection handling with debug and release URLs

--- client/global/global.gd
+++ client/global/global.gd
@@ -1,11 +1,15 @@
 extends Node
 
 const proto := preload("res://proto.gd")
-const server_url := "ws://127.0.0.1:8080"
+const debug_server_url := "ws://127.0.0.1:8080"
+const release_server_url := "wss://agarust-server.d8s.fun"
 
+var server_url := debug_server_url
 var connection_id := ""
 
 func _ready() -> void:
+	if not OS.is_debug_build():
+		server_url = release_server_url
 	WsClient.closed.connect(_on_ws_closed)
 
 func _on_ws_closed() -> void:
--- server/build.rs
+++ server/build.rs
@@ -1,5 +1,6 @@
-fn main() -> std::io::Result<()> {
+fn main() {
     std::env::set_var("OUT_DIR", "src/");
-    prost_build::compile_protos(&["../proto/packet.proto"], &["../proto/"])?;
-    Ok(())
+    if let Err(error) = prost_build::compile_protos(&["../proto/packet.proto"], &["../proto/"]) {
+        eprintln!("compile_protos error: {:?}", error);
+    }
 }
