29d9c6c337ed5c47cb9d2d822950ba66076f48ed
jerry <calljerryli@outlook.com>
2025-01-20 05:07:59 +0800

    fix: correct timestamp handling in ping functionality and adjust UI layout for better visibility

--- client/component/ping/ping.gd
+++ client/component/ping/ping.gd
@@ -10,12 +10,13 @@ func _on_ws_packet_received(packet: Global.proto.Packet) -> void:
 	if packet.has_ping():
 		var ping := packet.get_ping()
 		var client_timestamp := ping.get_client_timestamp()
-		var now := Time.get_unix_time_from_system() * 1000
+		var now := int(Time.get_unix_time_from_system() * 1000)
 		var diff := now - client_timestamp
 		text = "Ping: %s ms" % diff
 
 func _on_timer_timeout() -> void:
 	var packet := Global.proto.Packet.new()
 	var ping := packet.new_ping()
-	ping.set_client_timestamp(Time.get_unix_time_from_system() * 1000)
+	var now := int(Time.get_unix_time_from_system() * 1000)
+	ping.set_client_timestamp(now)
 	WsClient.send(packet)
