e4e60b1834a1f441567fa501db1987598cd5d278
jerry <calljerryli@outlook.com>
2025-01-28 13:27:38 +0800

    refactor: move server connection call to the correct position in _ready function

--- client/view/connecting/connecting.gd
+++ client/view/connecting/connecting.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
-	WsClient.connect_to_server(Global.server_url)
 	WsClient.connected.connect(_on_ws_connected)
 	WsClient.packet_received.connect(_on_ws_packet_received)
+	WsClient.connect_to_server(Global.server_url)
 
 func _on_ws_connected() -> void:
 	print_debug("server connected")
