9304d06504bae42cbe1b584e1ee1bf3134cfb4c0
Jerry <calljerryli@outlook.com>
2025-03-04 23:44:22 +0800

    feat: add rush particles and vfx pre-compile components for enhanced visual effects

--- client/global/ws_client.gd
+++ client/global/ws_client.gd
@@ -8,56 +8,56 @@ signal closed()
 signal packet_received(packet: Global.proto.Packet)
 
 func _physics_process(_delta: float) -> void:
-	_poll()
-	_update_state()
-	_read_data()
+    _poll()
+    _update_state()
+    _read_data()
 
 func _poll() -> void:
-	if socket.get_ready_state() != socket.STATE_CLOSED:
-		socket.poll()
+    if socket.get_ready_state() != socket.STATE_CLOSED:
+        socket.poll()
 
 func _update_state() -> void:
-	var state := socket.get_ready_state()
-	if last_state != state:
-		last_state = state
-		if state == socket.STATE_OPEN:
-			connected.emit()
-		elif state == socket.STATE_CLOSED:
-			closed.emit()
+    var state := socket.get_ready_state()
+    if last_state != state:
+        last_state = state
+        if state == socket.STATE_OPEN:
+            connected.emit()
+        elif state == socket.STATE_CLOSED:
+            closed.emit()
 
 func _read_data() -> void:
-	var packet := _get_packet()
-	if packet:
-		packet_received.emit(packet)
+    var packet := _get_packet()
+    if packet:
+        packet_received.emit(packet)
 
 func _get_packet() -> Global.proto.Packet:
-	if socket.get_available_packet_count() < 1:
-		return null
+    if socket.get_available_packet_count() < 1:
+        return null
 
-	var data := socket.get_packet()
+    var data := socket.get_packet()
 
-	var packet := Global.proto.Packet.new()
-	var result := packet.from_bytes(data)
-	if result != OK:
-		printerr("Error build packet from data: %s" % data)
-		return null
+    var packet := Global.proto.Packet.new()
+    var result := packet.from_bytes(data)
+    if result != OK:
+        printerr("Error build packet from data: %s" % data)
+        return null
 
-	return packet
+    return packet
 
 func connect_to_server(url: String, tls_options: TLSOptions = null) -> int:
-	socket = WebSocketPeer.new()
-	var err := socket.connect_to_url(url, tls_options)
-	if err != OK:
-		return err
+    socket = WebSocketPeer.new()
+    var err := socket.connect_to_url(url, tls_options)
+    if err != OK:
+        return err
 
-	last_state = socket.get_ready_state()
+    last_state = socket.get_ready_state()
 
-	return OK
+    return OK
 
 func send(packet: Global.proto.Packet) -> int:
-	var data := packet.to_bytes()
-	return socket.send(data)
+    var data := packet.to_bytes()
+    return socket.send(data)
 
 func close(code: int = 1000, reason: String = "") -> void:
-	socket.close(code, reason)
-	last_state = socket.get_ready_state()
+    socket.close(code, reason)
+    last_state = socket.get_ready_state()
