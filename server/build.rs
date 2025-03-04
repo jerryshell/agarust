db4e45a756687322fee7b10d5e05a9e7390394eb
Jerry <calljerryli@outlook.com>
2025-03-05 00:10:12 +0800

    chore: update build configuration and dependencies for 2024 edition

--- server/build.rs
+++ server/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    std::env::set_var("OUT_DIR", "src/");
+    unsafe { std::env::set_var("OUT_DIR", "src/") };
     if let Err(e) = prost_build::compile_protos(&["../proto/packet.proto"], &["../proto/"]) {
         eprintln!("compile_protos error: {:?}", e);
     }
